d37445e0a19f14c83835cf08b47895986da4d919
JayJeckel <jeckelmail@gmail.com>
2015-03-21 12:57:14 -0500

    Create IGuiActivatorId.java

---
+++ jeckelcorelibrary/api/guis/IGuiActivatorId.java
@@ -0,0 +1,11 @@
+package jeckelcorelibrary.api.guis;
+
+/**
+ * @deprecated Will be removed as its functionality is unused and unneeded.
+ */
+@Deprecated
+public interface IGuiActivatorId
+{
+	@Deprecated
+	public int getGuiId();
+}
